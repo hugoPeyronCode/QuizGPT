4c839a398647d8d71c402928f4aa0501dd10642c
Hugo Peyron <hugopeyron@gmail.com>
2023-11-02 18:24:52 +0100

    Initial Commit

---
+++ QuizGPT/QuizGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizGPTApp.swift
+//  QuizGPT
+//
+//  Created by Hugo Peyron on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
